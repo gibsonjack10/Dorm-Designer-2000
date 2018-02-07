b8930f57e42410d6cc66d82862493d861a306676
bennettmajer <35977336+bennettmajer@users.noreply.github.com>
2018-02-07 10:11:32 -0600

    Add files via upload
    passes all zybooks tests. needs commenting and header. we should both run through the code one more time.

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+
 
 //
 //
@@ -28,17 +28,16 @@ public class Main {
 	public static void setup(Data data) {
 		data.bedPositions = new float[6][2];
 		data.dragBedIndex = -1;
-//		for (int i=0; i<6; i++) {
-//			data.bedPositions[i][0] = -1;
-//			data.bedPositions[i][1] = -1;
-//		}
+		data.bedImage = data.processing.loadImage("images/bed.png");
+		
 		for (int i=0; i<6; i++) {
 			data.bedPositions[i] = null;
 		}
+		
 		data.backgroundImage = data.processing.loadImage("images/background.png");
-	    data.bedPositions[0] = new float[2];
-		data.bedPositions[0][0] = data.processing.width/2;
-		data.bedPositions[0][1] = data.processing.height/2;
+//	    data.bedPositions[0] = new float[2];
+//		data.bedPositions[0][0] = data.processing.width/2;
+//		data.bedPositions[0][1] = data.processing.height/2;
 		System.out.println("Press the \"B\" key to create a new bed");
 	}
 
@@ -49,39 +48,34 @@ public class Main {
 	public static void update(Data data) {
         data.processing.background(100,150,250);
 		data.processing.image(data.backgroundImage, data.processing.width/2, data.processing.height/2);
-		PImage bedImage = data.processing.loadImage("images/bed.png");
-		data.processing.image(bedImage, data.bedPositions[0][0], data.bedPositions[0][1]);
-				
+//		data.processing.image(data.bedImage, data.bedPositions[0][0], data.bedPositions[0][1]);
+		
+		for (int i=0; i<6; i++) {
+			if (data.bedPositions[i] != null) {
+				data.processing.image(data.bedImage, data.bedPositions[i][0], data.bedPositions[i][1]);
+			}
+		}
+	
+		
 		if (data.dragBedIndex != -1) {
-			data.processing.image(bedImage, data.processing.mouseX, data.processing.mouseY);
+			data.processing.image(data.bedImage, data.processing.mouseX, data.processing.mouseY);
 			data.bedPositions[data.dragBedIndex][0] = data.processing.mouseX;
 			data.bedPositions[data.dragBedIndex][1] = data.processing.mouseY;
-		} 
+		}	
 	}		
 			
 	
-	
-	
-	
-	
-///////
-/////// Ask about using data.bedImage.width and .height
-/////// Ask about null in float array
-///////
-	
-	
-	
-	
-	
-	
+		
 	public static void mouseDown(Data data) {
-		for (int i=0; i<1; i++) {
-			if (data.processing.mouseX > data.bedPositions[i][0] - 20 && //need to substitute data.bedImage.width and .height for the 20
-					data.processing.mouseX < data.bedPositions[i][0] + 20 &&
-					data.processing.mouseY > data.bedPositions[i][1] - 20 &&
-					data.processing.mouseY < data.bedPositions[i][1] + 20) {
+		for (int i=0; i<6; i++) {
+			if (data.bedPositions[i] == null) {
+				break;
+			}
+			if (data.processing.mouseX > data.bedPositions[i][0] - data.bedImage.width/2 && 
+					data.processing.mouseX < data.bedPositions[i][0] + data.bedImage.width/2 &&
+					data.processing.mouseY > data.bedPositions[i][1] - data.bedImage.height/2 &&
+					data.processing.mouseY < data.bedPositions[i][1] + data.bedImage.height/2) {
 				data.dragBedIndex = i;
-				System.out.println(i);
 				break;
 			} 		
 		}	
@@ -100,19 +94,20 @@ public class Main {
 	
 	
 	public static void keyPressed(Data data) {
-		Scanner scnr = new Scanner(System.in);
-		
-		int counter = 0;
-		System.out.println("Press the \"B\" key to create a new bed.");
-		if (scnr.nextLine().charAt(0) == 'B' || scnr.nextLine().charAt(0) == 'b') {
-			data.bedPositions[counter] = new float[2];
-			data.bedPositions[counter][0] = data.processing.width/2;
-			data.bedPositions[counter][1] = data.processing.height/2;
-			
-			counter++;
+		if (data.processing.key == 'B' || data.processing.key == 'b') {
+			for (int i=0; i<6; i++) {
+				if (data.bedPositions[i] == null) {
+					data.bedPositions[i] = new float[2];
+					data.bedPositions[i][0] = data.processing.width/2;
+					data.bedPositions[i][1] = data.processing.height/2;
+					break;
+				}
+			}
 		}
-		scnr.close();
 	}
+	
+	
+	
 	
 	
 	
